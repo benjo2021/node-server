b4a733a671ab1270588a0a73ea46b8984d61bfa1
Mack_Ignacio <mack_ignacio@yahoo.com>
2018-09-26 02:06:32 +0800

    Fix merge conflict

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import http from "http";
 import Server from "./server";
 
 class App {
-<<<<<<< HEAD
   constructor(private port?: number) {
     this.port = this.normalizeport(process.env.port || 3333);
   }
@@ -10,55 +9,27 @@ class App {
   async initializeApp() {
     try {
       const serverApp = Server.serverApp;
-      const app = http.createServer(serverApp).listen(this.port, () => this.onListen( app.address() ) );
+      const app = http.createServer(serverApp).listen(this.port, () => this.onListen(app.address()));
     } catch (error) {
       this.error(error);
       console.log(error);
-<<<<<<< HEAD
-      console.log(error);
-=======
->>>>>>> master
     }
   }
 
   onListen(address: any) {
-    console.log(`listening on ${this.port}`);
+    console.log(`Litening on ${this.port}`);
   }
 
   error(error: any) {
-    if ( error.syscall !== "listen" || error.code !== "EACCESS" || error.coden !== "ADDRINUSE") {
+    if (error.syscall !== "listen" || error.code !== "EACCES" || error.coden !== "EADDRINUSE") {
       throw error;
-=======
-    constructor(private port?: number) {
-        this.port = this.normalizeport(process.env.port || 3333);
     }
+  }
 
-    async initializeApp() {
-        try {
-            const serverApp = Server.serverApp;
-            const app = http.createServer(serverApp).listen(this.port, () => this.onListen(app.address()));
-        } catch (error) {
-            this.error(error);
-        }
-    }
-
-    onListen(address: any) {
-        console.log(`Litening on ${this.port}`);
-    }
-
-    error(error: any) {
-        if (error.syscall !== "listen" || error.code !== "EACCES" || error.coden !== "EADDRINUSE") {
-            throw error;
-        }
-    }
-
-    normalizeport(num: any) {
-        const port = parseInt(num, 10);
-        return isNaN(port) ? num : port;
->>>>>>> 1f4bd1b7113f5e591cef6577bb6b167a358ee447
-    }
-
-
+  normalizeport(num: any) {
+    const port = parseInt(num, 10);
+    return isNaN(port) ? num : port;
+  }
 }
 
 new App().initializeApp();
--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
-import express from 'express';
-=======
 import express from "express";
->>>>>>> 1f4bd1b7113f5e591cef6577bb6b167a358ee447
 
 class Routes {
-    constructor(serverApp: express.Application) {
-        console.log("test");
-    }
+  constructor(serverApp: express.Application) {
+    console.log("test");
+  }
 }
 
 export default Routes;
