09f9aeaa0e7b6612449b6f99c13053abf34ae231
Mack_Ignacio <mack_ignacio@yahoo.com>
2018-09-29 05:04:52 +0800

    Refactor some codes

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,59 @@
-import http from "http";
-import Server from "./server";
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import { User } from "./model/user";
 
-class App {
-  constructor(private port?: number) {
-    this.port = this.normalizeport(process.env.port || 3333);
-  }
+const app = express(),
+  user = new User();
+app.use(express.static(__dirname + "/client"));
+app.use(bodyParser.json());
 
-  async initializeApp() {
-    try {
-      const serverApp = Server.serverApp;
-      const app = http.createServer(serverApp).listen(this.port, () => this.onListen(app.address()));
-    } catch (error) {
-      this.error(error);
-      console.log(error);
+// Connect to Mongoose
+mongoose.connect("mongodb://localhost/test");
+const db = mongoose.connection;
+
+app.get("/", (req, res) => {
+  res.status(404).send("Invalid route/s!!!");
+});
+
+app.get("/api/user", (req, res) => {
+  user.getUsers((err: any, user: any) => {
+    if (err) {
+      throw err;
     }
-  }
+    res.json(user);
+  });
+});
 
-  onListen(address: any) {
-    console.log(`Litening on ${this.port}`);
-  }
-
-  error(error: any) {
-    if (error.syscall !== "listen" || error.code !== "EACCES" || error.coden !== "EADDRINUSE") {
-      throw error;
+app.post("/api/user", (req, res) => {
+  var User = req.body;
+  user.addUsers(User, (err: any, user: any) => {
+    if (err) {
+      throw err;
     }
-  }
+    res.json(user);
+  });
+});
 
-  normalizeport(num: any) {
-    const port = parseInt(num, 10);
-    return isNaN(port) ? num : port;
-  }
-}
+app.put("/api/user/:_id", (req, res) => {
+  var id = req.params._id;
+  var User = req.body;
+  user.updateUsers(id, User, {}, (err: any, user: any) => {
+    if (err) {
+      throw err;
+    }
+    res.json(user);
+  });
+});
 
-new App().initializeApp();
+/* app.delete("/api/user/:_id", (req, res) => {
+  var id = req.params._id;
+  user.removeUser(id, (err: any, user: any) => {
+    if (err) {
+      throw err;
+    }
+    res.json(User);
+  });
+}); */
+
+app.listen(3000, () => console.log("Listening on port 3000..."));
